8fe5077e32597f3f012283f0a513b6825ff8ca1e
Victor <victorsntnf@gmail.com>
2021-05-31 20:42:04 -0300

    atualização codigo

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(express.json())
 app.use(cors())
 
 app.get("/", (req, res) => {
-    res.send("Parabéns você chegou até aqui.")
+    res.send("Bem-vindo, chegamos até esse ponto.")
 })
 
 require("./controllers/login")(app)
